4f3b51c584a68d2ef77fd85c51a09e33783171f9
nedilio <izquierdonelson@gmail.com>
2016-11-03 16:07:13 -0400

    quitando dist del gitignore

--- templates/content-single-medicina-prepagada.php
+++ templates/content-single-medicina-prepagada.php
@@ -3,13 +3,25 @@
 
     <div class="entry-content content-medicina-prepagada">
     <header class="menu-med-prep">
-      aqui deberia el menu para navegar los singles
+
+<ul class="list-inline">
+    <?php $loop = new WP_Query( array( 'post_type' => 'medicina-prepagada', 'posts_per_page' => -1 ) ); ?>
+<?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+  <li>Plan <span class="text-uppercase"><?php the_title(); ?></span></li>
+  <?php endwhile; wp_reset_query(); ?>
+</ul>
+
     </header>
 
     <div class="row">
       <div class="col-sm-7">
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
+        <h2>Plan <span class="text-uppercase"><?php the_title(); ?></span></h2>
+        <div class="wrapper-med-prep"><?php the_content(); ?></div>
+        <div class="wrapper-botones text-center">
+          <a href="#" class="boton-med-prep">Centros de Salud</a>
+          <a href="#" class="boton-med-prep">Cotización Familiar</a>
+          <a href="#" class="boton-med-prep">Cotizar Empresa</a>
+        </div>
       </div>
       <div class="col-sm-5">
         javier
